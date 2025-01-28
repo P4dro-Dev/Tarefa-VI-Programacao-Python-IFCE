845d29ac2324eae1c6d7b31e2aa0b05fdf149c1d
Pedro Henrique <Ph9084533@gmail.com>
2025-01-28 16:32:28 +0000

    Atualizando!

---
+++ Exercício-VII-Python-IFCE/Q3.py
@@ -0,0 +1,6 @@
+# Implemente um programa que, ao receber o dia da semana (como texto), informe se um serviço está
+# disponível. Use as seguintes condições:
+# • De Segunda a sexta-feira: "Atendimento disponível das 9h às 18h."
+# • Sábado: "Atendimento disponível das 9h às 12h."
+# • Domingo: "Serviço indisponível."
+# • Se o dia informado não for válido, o programa deve exibir uma mensagem de erro.
