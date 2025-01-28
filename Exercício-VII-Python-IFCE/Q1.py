910d1ede638d33d2f036d5639c8cfe72ab8dbb38
Pedro Henrique <Ph9084533@gmail.com>
2025-01-28 16:31:19 +0000

    Adicionando comentários nas questões de Python

---
+++ Exercício-VII-Python-IFCE/Q1.py
@@ -0,0 +1,10 @@
+# Escreva um programa para ler o peso de uma pessoa na Terra e o número de um planeta. Em seguida, o
+# programa deve exibir o peso equivalente da pessoa no planeta escolhido. (Obs: Resolva utilizando a
+# estrutura match-case)
+
+# ID Planeta Gravidade
+# 1 Mercúrio 0,37
+# 2 Vênus 0,88
+# 3 Marte 0,38
+# 4 Júpiter 2,64
+# 5 Saturno 1,15
---
+++ Exercício-VII-Python-IFCE/Q2.py
@@ -0,0 +1,20 @@
+# Desenvolva um programa que leia o código de um determinado produto e mostre sua classificação.
+# Utilize a seguinte tabela como referência: (Obs: Resolva utilizando a estrutura match-case)
+
+# Código Classificação
+# 1 Alimento não-perecível
+# 2, 3 ou 4 Alimento perecível
+# 5 ou 6 Vestuário
+# 7 Higiene pessoal
+# 8, 9 ou 10 Limpeza e utensílios domésticos
+# Qualquer outro código Código invalido
+
+# Entrada
+# Um número inteiro correspondendo ao código de um produto.
+# Saída
+# A classificação do produto, conforme a tabela apresentada anteriormente.
+# CURSO: TÉCNICO EM INFORMÁTICA
+# DISCIPLINA: INTRODUÇÃO À PROGRAMAÇÃO
+
+# ALUNO(A):
+
